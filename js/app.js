a76ccd5b285d403f347aafc1bcb4b84477897de9
Nehemias Alcantara <nehemiasalcantara@Nehemiass-MacBook-Air.local>
2020-01-02 10:57:03 -0600

    user can throw the ball

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
 console.log('basketbal game');
+
+class Basketball {
+	constructor(ball) {
+		this.ball = ball
+		hitTarget = false
+	}
+}
+
+const game = {
+	remainingAttemp: 2,
+	points: 0,
+	throwBall() {
+		$('.ball').animate({ 
+		bottom:'+=80%',
+		width: '-=6%',
+		height: '-=6%'
+	}, 600);
+	$('.ball').animate({ top: '+=40%'}, 800)
+	}
+}
+
+
+// Listeners
+$('.ball').click(function() {
+	game.throwBall()
+});
